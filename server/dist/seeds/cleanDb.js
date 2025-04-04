cc54a246b92d323ce6e85ec4a9918d6e81fdf5ca
Diana-Khalina <diaaana@i.ua>
2025-04-03 23:12:38 -0400

    adding code

---
+++ server/dist/seeds/cleanDb.js
@@ -0,0 +1,17 @@
+import models from '../models/index.js';
+import db from '../config/connection.js';
+export default async (modelName, collectionName) => {
+    try {
+        // Use optional chaining to safely access db properties
+        let modelExists = await models[modelName]?.db?.db?.listCollections({
+            name: collectionName
+        }).toArray();
+        // If modelExists is not null or undefined, proceed to drop the collection
+        if (modelExists?.length) {
+            await db.dropCollection(collectionName);
+        }
+    }
+    catch (err) {
+        throw err;
+    }
+};
--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,14 +3,16 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
+    // Use optional chaining to safely access db properties
+    let modelExists = await models[modelName]?.db?.db?.listCollections({
       name: collectionName
-    }).toArray()
+    }).toArray();
 
-    if (modelExists.length) {
+    // If modelExists is not null or undefined, proceed to drop the collection
+    if (modelExists?.length) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
     throw err;
   }
-}
+};
